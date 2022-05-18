27bcd39b6d036dea6d5206332819cffc9ac02029
Tishtanya Shaw <Tishtanyaa@icloud.com>
2022-05-18 16:58:00 -0400

    Add Oauth login functionality

--- apps/app/src/app/components/Login.tsx
+++ apps/app/src/app/components/Login.tsx
@@ -1,8 +1,17 @@
+import { environment } from "../../environments/environment";
+
 export function Login() {
+    const clientId = environment.clientId;
+    const scopes = 'user:email';
+    const redirectUri = `${environment.apiUri}/github/authorize`;
+    const ghOauthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`
     return (
-        <div>
-            <h2>Login</h2>
+        <div className="p-2 flex flex-col m-auto w-full lg:w-1/3">
+            <h2 className="my-4 text-3xl font-black uppercase">Login</h2>
+            <a href={ghOauthUrl} className="bg-black text-white p-2 font-bold w-full gap-2 flex items-center justify-center">
+                <img src="../../assets/GitHub-Mark-Light-32px.png" alt="github logo" />
+                <span className="">Login with Github</span>
+            </a>
         </div>
     );
 }
-
--- apps/app/src/environments/environment.ts
+++ apps/app/src/environments/environment.ts
@@ -3,4 +3,6 @@
 
 export const environment = {
   production: false,
+  clientId: '7964294513a98d5cf05c',
+  apiUri: 'http://localhost:3333'
 };
